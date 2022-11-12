381db349a27d02273ce184a93b43054c64131036
Tyson R <tyson.r@ens.uvsq.fr>
2022-11-12 23:01:46 +0100

    TD

---
+++ exercises/TD04_listes/carre_magique.py
@@ -0,0 +1,12 @@
+carre_mag=[[4,14,15,1],[9,7,6,12],[5,11,10,8],[16,2,3,13]]
+carre_pas_mag=[[4,14,15,1],[9,7,6,12],[5,11,10,8],[16,2,7,13]]
+
+def afficheCarre(carre):
+    a=print("",carre[0],"\n",carre[1],"\n",carre[2],"\n",carre[3])
+    return a
+
+afficheCarre(carre_mag)  
+afficheCarre(carre_pas_mag)
+
+def testLignesEgales(carre):
+    pass
