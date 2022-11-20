a6a7e5e41c0cfadc9ece572ab19639a5666fbbea
Tyson R <tyson.r@ens.uvsq.fr>
2022-11-20 13:44:46 +0100

    TD

--- exercises/TD04_listes/carre_magique.py
+++ exercises/TD04_listes/carre_magique.py
@@ -2,7 +2,8 @@ carre_mag=[[4,14,15,1],[9,7,6,12],[5,11,10,8],[16,2,3,13]]
 carre_pas_mag=[[4,14,15,1],[9,7,6,12],[5,11,10,8],[16,2,7,13]]
 
 def afficheCarre(carre):
-    a=print("",carre[0],"\n",carre[1],"\n",carre[2],"\n",carre[3])
+    for i in carre:
+        a=print(i[0:4])
     return a
 
 afficheCarre(carre_mag)  
@@ -29,26 +30,52 @@ print(testLignesEgales(carre_mag))
 print(testLignesEgales(carre_pas_mag))
 
 def testColonnesEgales(carre):
-    b=[a[0][0],a[1][0]]
-    a=carre
     s,sa,sb,sc=0,0,0,0
-    for i in a:
-        for j in i:
-            print(j)
-    
-        return j
+    a=len(carre)
+    for i in carre:
+        s+=i[0]
+        sa+=i[1]
+        sb+=i[2]
+        sc+=i[3]
+    if s==sa==sb==sc:
+        return s
+    else:
+        return -1
     
 print(testColonnesEgales(carre_mag))
+print(testColonnesEgales(carre_pas_mag))
 
+def testDiagonalesEgales(carre):
+    b=[]
+    c=[]
+    i,j,n=0,0,3
+    sg,sd=[],[]
+    for a in range(4):#gauche
+        b.append(carre[i])
+        sg.append(b[i][i])
+        i+=1    
+    #print(sg)
+    dg,dd=0,0
+    for g in sg:
+        dg+=g
+    for j in range(4):
+        c.append(carre[j])
+        sd.append(c[j][n])
+        j+=1
+        n-=1
+    #print(sd)
+    for h in sd:
+        dd+=h
+    if dg==dd:
+        return print("La somme des valeurs de la diagonale gauche est égale à",dg,"et",dd,"pour celle de droite.")
+    else:
+        return -1
+print(testDiagonalesEgales(carre_mag))
 
-
-
-
-
-
-
-
-
-
-
-
+def estCarreMagique(carre):
+    if testLignesEgales(carre)==True:
+        print("yes")
+    else:
+        False
+    return
+print(estCarreMagique(carre_mag))
